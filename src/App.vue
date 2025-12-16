cd24a006143a1dcb70c41f1c2d472ad2ab41d202
Leslie Fernandez <lesliefernandez10.lf@gmail.com>
2025-12-16 11:20:12 -0500

    Create test call to location API in App to see how it works

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { getUserLocation } from './services/location';
+import type { LocationData } from './types/types';
+
+const location = ref<LocationData | null>(null);
+
+async function loadLocationData() {
+  try {
+    const locationData = await getUserLocation();
+    location.value = locationData;
+    console.log(location.value);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+onMounted(() => {
+  loadLocationData();
+});
+</script>
 
 <template>
-  <div></div>
+  <div>Hi again</div>
 </template>
 
 <style scoped></style>
